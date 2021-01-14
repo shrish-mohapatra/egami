5640731d3fc5f408d2708da1190660707db6d0c3
shrish-mohapatra <shrish.mohapatra@gmail.com>
2021-01-14 17:13:13 -0500

    Started node app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 5000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.listen(port, () => {
+    console.log("The server is running on port", port)
+})
